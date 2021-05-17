d437ebfb7e7a8da3739edcba1f5b8cffc9ef813a
Brian Kurosawa <brian.kurosawa@gmail.com>
2021-05-17 04:07:33 -0300

    update script.js: add validador de cpf

--- script.js
+++ script.js
@@ -45,3 +45,52 @@ function validaTelefone(campo) {
         ocultaErro(campo);
     }
 }
+
+function validaCpf(campo) {
+    let valor = document.getElementById(campo).value;
+    if(testaCpf(valor)) {
+        ocultaErro(campo);
+    }
+    else {
+        mostraErro(campo, "Cpf inválido");
+    }
+}
+
+function testaCpf(cpf) {
+    let valor = cpf;
+    if(!valor.match(/\d{11}/)) return false;
+    let digitosIguais = true;
+    let numeros = "";
+    let digitos = "";
+    let soma = 0;
+    let resultado = 0;
+    for(i = 0; i < (valor.length - 1); i++) {
+        if(valor.charAt(i) != valor.charAt(i + 1)) {
+            digitosIguais = false;
+            break;
+        }
+    }
+    if(!digitosIguais) {
+        numeros = cpf.substring(0, 9);
+        digitos = cpf.substring(9);
+        for(j = 10; j > 1; j--) {
+            soma += numeros.charAt(10 - j) * j;
+        }
+        resultado = soma % 11;
+        resultado = (resultado < 2) ? 0 : 11 - resultado;
+        if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+        numeros = valor.substring(0, 10);
+        soma = 0;
+        for(k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+        resultado = soma % 11;
+        resultado = (resultado < 2) ? 0 : 11 - resultado;
+    }
+    else {
+        return false;
+    }
+    return true;
+}
